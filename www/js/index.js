b058cf9ad6a70918b1a36e50bbe81ba215b9f932
matheuscoelho <matheuscoelhoo321@gmail.com>
2017-11-19 07:46:53 -0200

    3
    3

--- www/js/index.js
+++ www/js/index.js
@@ -69,6 +69,8 @@ var app = {
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
+
+            receivedElement.value(data.registrationId);
         });
 
         push.on('error', function(e) {
